vponomaryov/rally

[Manila] Add benchmark for Manila list share servers operation
List of changes:
- Added support of 'list' operation for share servers.
- Added benchmark for listing of share servers.

Change-Id: Iea34c51eaf3cf73de52f882b09592a793a3159c1

rally/plugins/openstack/scenarios/manila/utils.py
@@ -159,3 +159,15 @@ class ManilaScenario(base.Scenario):
         share_networks = self.clients("manila").share_networks.list(
             detailed=detailed, search_opts=search_opts)
         return share_networks
+
+    @base.atomic_action_timer("manila.list_share_servers")
+    def _list_share_servers(self, search_opts=None):
+        """List share servers. Admin only.
+
+        :param search_opts: set of key-value pairs to filter share servers by.
+            Example: {"share_network": "share_network_name_or_id"}
+        :returns: list of instances of :class:`ShareServer`
+        """
+        share_servers = self.clients("manila").share_servers.list(
+            search_opts=search_opts)
+        return share_servers

openstack/rally-openstack

rally/plugins/openstack/scenarios/manila/utils.py
@@ -160,3 +160,15 @@ class ManilaScenario(base.Scenario):
         share_networks = self.clients("manila").share_networks.list(
             detailed=detailed, search_opts=search_opts)
         return share_networks
+
+    @base.atomic_action_timer("manila.list_share_servers")
+    def _list_share_servers(self, search_opts=None):
+        """List share servers. Admin only.
+
+        :param search_opts: set of key-value pairs to filter share servers by.
+            Example: {"share_network": "share_network_name_or_id"}
+        :returns: list of instances of :class:`ShareServer`
+        """
+        share_servers = self.admin_clients("manila").share_servers.list(
+            search_opts=search_opts)
+        return share_servers