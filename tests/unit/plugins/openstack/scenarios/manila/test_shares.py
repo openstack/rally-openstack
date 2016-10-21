vponomaryov/rally

[Manila] Add create_and_list_share scenario for Manila
Add 'create_and_list_share' scenario and task samples and update manila
tasks for CI.

Change-Id: I9317e40f8f62eb84d26833f7efbe32337cdfb770

tests/unit/plugins/openstack/scenarios/manila/test_shares.py
@@ -209,3 +209,21 @@ class ManilaSharesTestCase(test.TestCase):
         scenario._add_security_service_to_share_network.assert_has_calls(
             mock.call(scenario._create_share_network.return_value,
                       scenario._create_security_service.return_value))
+
+    @ddt.data(
+        {"share_proto": "nfs", "size": 3, "detailed": True},
+        {"share_proto": "cifs", "size": 4, "detailed": False,
+         "share_network": "foo", "share_type": "bar"},
+    )
+    def test_create_and_list_share(self, params):
+        scenario = shares.ManilaShares()
+        scenario._create_share = mock.MagicMock()
+        scenario.sleep_between = mock.MagicMock()
+        scenario._list_shares = mock.MagicMock()
+
+        scenario.create_and_list_share(min_sleep=3, max_sleep=4, **params)
+
+        detailed = params.pop("detailed")
+        scenario._create_share.assert_called_once_with(**params)
+        scenario.sleep_between.assert_called_once_with(3, 4)
+        scenario._list_shares.assert_called_once_with(detailed=detailed)

openstack/rally-openstack

tests/unit/plugins/openstack/scenarios/manila/test_shares.py
@@ -191,3 +191,21 @@ class ManilaSharesTestCase(test.ScenarioTestCase):
         scenario._add_security_service_to_share_network.assert_has_calls([
             mock.call(scenario._create_share_network.return_value,
                       scenario._create_security_service.return_value)])
+
+    @ddt.data(
+        {"share_proto": "nfs", "size": 3, "detailed": True},
+        {"share_proto": "cifs", "size": 4, "detailed": False,
+         "share_network": "foo", "share_type": "bar"},
+    )
+    def test_create_and_list_share(self, params):
+        scenario = shares.CreateAndListShare()
+        scenario._create_share = mock.MagicMock()
+        scenario.sleep_between = mock.MagicMock()
+        scenario._list_shares = mock.MagicMock()
+
+        scenario.run(min_sleep=3, max_sleep=4, **params)
+
+        detailed = params.pop("detailed")
+        scenario._create_share.assert_called_once_with(**params)
+        scenario.sleep_between.assert_called_once_with(3, 4)
+        scenario._list_shares.assert_called_once_with(detailed=detailed)