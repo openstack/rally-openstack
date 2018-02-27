209337291c50bed5c7ef03a1169f8de4672ef99f
Andrey Kurilin <andr.kurilin@gmail.com>
2018-02-27 15:23:26 +0200

    Add basic job for Rally task
    * Rename zuul.yaml -> .zuul.yaml to hide this file from the top level of
      root directory
    
    * Remove all redundant scripts which were used in the old CI
    
    * Modify devstack plugin to print information about Rally-OpenStack
    
    * Add simple task file with one workload to check the single job
    
    * Add an index html page for the new job. It is quite simple, doesn't
      use template, so no need to special script which will make this file.
      Also, we do not need to ship this file with python package since it is
      located in proper place.
    
    * Add a basic job which launchs Rally task based on input arguments:
      - rally_task_file - a patch to Rally task file in relative way to
           project path (project which where the job is launched)
      - rally_task_args_file - an optional path to a file with arguments to
           Rally task. It is also relative to the project path
      - use_existing_users - Defaults to False. Whethere to create and put
           existing users to Rally deployment/enviroment or not.
    
    Change-Id: Ib3b021914ac7d565df05c5397b378037ca0c0b00

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -8,13 +8,13 @@ DIR=$(dirname ${BASH_SOURCE[0]})
 source $DIR/lib/rally
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
-    echo_summary "Installing Rally"
+    echo_summary "Installing Rally-OpenStack"
     install_rally
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-    echo_summary "Configuring Rally"
+    echo_summary "Configuring Rally-OpenStack"
     configure_rally
 elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
-    echo_summary "Initializing Rally"
+    echo_summary "Initializing Rally-OpenStack"
     init_rally
 fi
 
